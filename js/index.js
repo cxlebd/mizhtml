61d5ff3a1bec0b375f89f4a9d5b29645c08d58f3
cxlebd <dsanchezg1903@alumno.ipn.mx>
2022-04-20 10:45:41 -0500

    commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+window.sr = ScrollReveal();
+
+ScrollReveal().reveal('.index_card', {
+    duration: 3500,
+    reset: true,
+    delay: 450
+});
+
+ScrollReveal().reveal('.nav_inicio', {
+    duration: 3500
+})
